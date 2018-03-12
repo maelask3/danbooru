26ccb5611ca2e8b54632bfc7270ddc26dcf5ad77
Meoberto <meoberto@mthree.es>
2018-03-12 22:37:46 +0000

    i don't know what the fuck i'm doing

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,6 @@
 set :user, "albert"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "danbooru.mthree.es", :roles => %w(web app db), :primary => true, :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
-set :rbenv_path, "/home/albert/.rbenv"
+set :rbenv_path, "/home/danbooru/.rbenv"
