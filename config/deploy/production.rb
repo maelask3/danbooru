9a795679c7ac22992684939f549b2bc4f8cd8476
Meoberto <meoberto@mthree.es>
2018-05-05 14:54:06 +0000

    That's not how it works apparently

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,6 @@
 set :user, "danbooru"
 set :rails_env, "production"
 server "danbooru.mthree.es", :roles => %w(web app), :primary => true, :user => "danbooru"
-server "db02.ux.mthree.es", :roles => %w(db), :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
 set :rbenv_path, "/home/danbooru/.rbenv"
