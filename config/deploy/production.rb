050aa10c1fbdf59e2e4119692b30ef95b7f65382
Meoberto Whagbossa <meoberto@mthree.es>
2018-05-13 18:58:51 +0000

    Unscrewed configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "danbooru.mthree.es", :roles => %w(web app), :primary => true, :user => "danbooru"
+server "danbooru.mthree.es", :roles => %w(web app db), :primary => true, :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
 set :rbenv_path, "/home/danbooru/.rbenv"
