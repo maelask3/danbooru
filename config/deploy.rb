198d1671147410be42c4ffb0a233a34d2a22f3cc
Meoberto Whagbossa <meoberto@mthree.es>
2018-10-28 21:22:54 +0000

    I think I fixed the things

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,7 @@ set :default_stage, "staging"
 set :application, "danbooru"
 set :repo_url,  "git://github.com/maelask3/danbooru.git"
 set :scm, :git
-<<<<<<< HEAD
-set :deploy_to, "/var/www/danbooru2"
-=======
 set :deploy_to, "/var/www/danbooru"
->>>>>>> 271c50d1c88315d35d2b3161d0c7559001398455
 set :rbenv_ruby, "2.5.1"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 set :branch, ENV.fetch("branch", "master")
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,16 +1,6 @@
 set :user, "danbooru"
 set :rails_env, "production"
-<<<<<<< HEAD
 server "danbooru.mthree.es", :roles => %w(web app db), :primary => true, :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
 set :rbenv_path, "/home/danbooru/.rbenv"
-=======
-server "kagamihara", :roles => %w(web app db), :primary => true
-server "shima", :roles => %w(web app)
-server "saitou", :roles => %w(web app)
-server "oogaki", :roles => %w(worker)
-
-set :linked_files, fetch(:linked_files, []).push(".env.production")
-set :rbenv_path, "/home/danbooru/.rbenv"
->>>>>>> 271c50d1c88315d35d2b3161d0c7559001398455
